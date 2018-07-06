d04bef409013a665929a68d3651fc5c831464ef0
Georgy Petrukhin <mumbasa@gmail.com>
2018-07-06 15:29:33 +1200

    LIGHT#2

--- TwitterProject/urls.py
+++ TwitterProject/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.views.generic.base import TemplateView
+from apptwitter.views import addMessage
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
     re_path(r'^accounts/', include('allauth.urls')),
-    path('', TemplateView.as_view(template_name='user/main.html')),
+    path('', addMessage),
     path('auth', TemplateView.as_view(template_name='user/auth.html')),
     # re_path(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
     # re_path(r'^$', TemplateView.as_view(template_name='plain/example/index.html'), name='home'),
--- apptwitter/views.py
+++ apptwitter/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from apptwitter.models import Messages
 
-# Create your views here.
+
+# Сохраняем данные из формы в бaзу данных
+def addMessage(request):
+    context = articles()
+    if request.method == 'POST':
+        addMessage = request.POST.get('Textarea1', '')
+        if str(addMessage) != '':
+            i = Messages.objects.create(text=addMessage)
+            i.save()
+    else:
+        pass
+    return render(request, 'user/main.html', context)
+
+
+# Редактируем данные из базы
+def articles():
+    y = Messages.objects.values_list().values()
+    messageslist = {
+        'context_value': y,
+    }
+    return messageslist
